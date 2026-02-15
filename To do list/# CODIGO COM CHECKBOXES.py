dc4ba8892206e35f80ceae3eb97e8a6ca5f9e233
ruilopes14 <rui.d.lopes@outlook.pt>
2026-02-15 16:23:52 +0100

    Delete To do list directory

--- To do list/# CODIGO COM CHECKBOXES.py
+++
@@ -1,7 +0,0 @@
-import json
-
-guardar = [ {"tarefa": "Comprar pão", "completa": False},]
-
-with open ("data89.json", "w" ) as f:
-    json.dump( guardar, f, indent=4)
-    print(json.dumps(guardar, indent=4))
